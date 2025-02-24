56976ae74dc07b09d8a4e6459a19ea595460598f
Helen Svanberg <helen.svanberg@gmail.com>
2025-02-24 11:03:22 +0100

    Modularised into primes.h and .c files

---
+++ include/primes.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#ifndef _PRIMES_H
+#define _PRIMES_H
+
+char *iarr_to_str(char *buf, int *iarr, int ialen);
+bool is_prime(int num);
+int get_next_prime(int prime);
+int *prime_factors(int *buffer, int *factors, int num);
+#endif
